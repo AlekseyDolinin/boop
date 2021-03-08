d67659958e28595731c454698b66ec310ab9ff5a
AlekseyDolinin <allDolinin@gmail.com>
2021-03-08 14:19:18 +0300

    поправлена верстка

--- Boop/Controllers/DelegatesStartVC.swift
+++ Boop/Controllers/DelegatesStartVC.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-extension StartViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension StartViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return arrayKeysServices.count
@@ -26,38 +26,7 @@ extension StartViewController: UICollectionViewDelegate, UICollectionViewDataSou
         }
     }
     
-    /// центрирование ячеек
-//    func centerItemsInCollectionView(cellWidth: Double, numberOfItems: Double, spaceBetweenCell: Double, collectionView: UICollectionView) -> UIEdgeInsets {
-//        let totalWidth = cellWidth * numberOfItems
-//        let totalSpacingWidth = spaceBetweenCell * (numberOfItems - 1)
-//        let leftInset = (collectionView.frame.width - CGFloat(totalWidth + totalSpacingWidth)) / 2
-//        let rightInset = leftInset
-//        return UIEdgeInsets(top: 0, left: leftInset, bottom: 0, right: rightInset)
-//    }
-    
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-//
-//        guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout,
-//            let dataSourceCount = collectionView.dataSource?.collectionView(collectionView, numberOfItemsInSection: section),
-//            dataSourceCount > 0 else {
-//                return .zero
-//        }
-//
-//        let cellCount = CGFloat(dataSourceCount)
-//        let itemSpacing = flowLayout.minimumInteritemSpacing
-//        let cellWidth = flowLayout.itemSize.width + itemSpacing
-//        var insets = flowLayout.sectionInset
-//
-//        let totalCellWidth = (cellWidth * cellCount) - itemSpacing
-//        let contentWidth = collectionView.frame.size.width - collectionView.contentInset.left - collectionView.contentInset.right
-//
-//        guard totalCellWidth < contentWidth else {
-//            return insets
-//        }
-//
-//        let padding = (contentWidth - totalCellWidth) / 2.0
-//        insets.left = padding
-//        insets.right = padding
-//        return insets
-//    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
 }
