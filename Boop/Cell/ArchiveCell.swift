e37236b618c78d60d9148852571b2f6343acf919
allDolinin <allDolinin@gmail.com>
2021-11-10 12:01:21 +0300

    1

--- Boop/Cell/ArchiveCell.swift
+++ Boop/Cell/ArchiveCell.swift
@@ -1,4 +1,6 @@
 import UIKit
+import SwiftLinkPreview
+import Kingfisher
 import FaviconFinder
 
 class ArchiveCell: UITableViewCell {
@@ -9,6 +11,8 @@ class ArchiveCell: UITableViewCell {
     @IBOutlet weak var shortlink: UILabel!
     @IBOutlet weak var longlink: UILabel!
     @IBOutlet weak var favicon: UIImageView!
+    //    @IBOutlet weak var imagePreview: UIImageView!
+    //    @IBOutlet weak var loader: UIActivityIndicatorView!
     
     var archiveItem: ArchiveItem!
     
@@ -23,21 +27,21 @@ class ArchiveCell: UITableViewCell {
         setDate()
         setName()
         getFavicon()
-    }
-    
-    func setName() {
-        name.text = archiveItem.name
+        parseHTML()
     }
     
     func setShortLink() {
         shortlink.text = archiveItem.shortLink
     }
     
+    func setName() {
+        name.text = archiveItem.name
+    }
+    
     func setLongtLink() {
         longlink.text = archiveItem.longLink
     }
     
-    ///
     func getFavicon() {
         guard let url: URL = URL(string: archiveItem.longLink) else {return}
         FaviconFinder(url: url).downloadFavicon { result in
@@ -52,11 +56,32 @@ class ArchiveCell: UITableViewCell {
         }
     }
     
+    ///
     func setDate() {
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .long
         dateFormatter.timeStyle = .short
         dateLabel.text = dateFormatter.string(from: archiveItem.date)
+    }
+    
+    
+    func parseHTML() {
+        let slp = SwiftLinkPreview(session: URLSession.shared,
+                                   workQueue: SwiftLinkPreview.defaultWorkQueue,
+                                   responseQueue: DispatchQueue.main,
+                                   cache: DisabledCache.instance)
+        slp.preview(archiveItem.longLink,
+                    onError: { error in
+            print("Error: \(error)")},
+                    onSuccess: { result in
+            print("canonicalUrl:\(result.canonicalUrl)")
+            print("finalUrl:\(result.finalUrl)")
+            print("description:\(result.description)")
+            print("url:\(result.url)")
+            print("title:\(result.title)")
+            print("favicon:\(result.icon)")
+            print("imagePreview:\(result.image)")
+        })
     }
 }
 
@@ -64,5 +89,28 @@ extension ArchiveCell {
     func setUI() {
         backView.layer.cornerRadius = 8.0
         backView.clipsToBounds = true
+        backView.backgroundColor = .clear
+    }
+}
+
+import UIKit
+import Foundation
+
+class loadImage {
+    class func get(urlImage: URL, completion: @escaping (UIImage?) -> ()) {
+        UIImageView().kf.setImage(with: urlImage,
+                                  placeholder: UIImage(named: ""),
+                                  options: [ .scaleFactor(UIScreen.main.scale),
+                                             .transition(.fade(0.2)),
+                                             .cacheOriginalImage]) { receivedSize, totalSize in
+        } completionHandler: { image, error, cacheType, imageURL in
+            if error != nil {
+                print("ERROR GET IMAGE: \(String(describing: error))")
+                print("ERROR LINK: \(String(describing: imageURL))")
+                completion (UIImage(named: "logo_frame"))
+            } else {
+                completion (image)
+            }
+        }
     }
 }
