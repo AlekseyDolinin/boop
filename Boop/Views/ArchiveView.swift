d693ae4fa61e0bb087400c0082f36880c77bacba
allDolinin <allDolinin@gmail.com>
2021-11-15 10:39:00 +0300

    Поправлен перевод

--- Boop/Views/ArchiveView.swift
+++ Boop/Views/ArchiveView.swift
@@ -5,29 +5,25 @@ class ArchiveView: UIView {
     @IBOutlet weak var archiveTable: UITableView!
     @IBOutlet weak var emptyLabel: UILabel!
     @IBOutlet weak var backButton: UIButton!
-
     
     override func awakeFromNib() {
-
         setUI()
-        
-    }
-    
-    
-    func configure() {
-
     }
 }
 
 extension ArchiveView {
-    
+    ///
     func setUI() {
-        
+        setLabels()
         ///
         backButton.setImage(UIImage(named: "backArrow")?.withRenderingMode(.alwaysTemplate), for: .normal)
         backButton.tintColor = .black
         
         ///
         emptyLabel.isHidden = true
+    }
+    ///
+    func setLabels() {
+        emptyLabel.text = AppLanguage.dictionary["archiveIsEmpty"]!.stringValue
     }
 }
